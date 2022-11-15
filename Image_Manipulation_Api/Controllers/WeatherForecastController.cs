1292ee96d9bcdad1fbd8e54300bb6835d09e739f
Abdur Rahman Mashoor Ali <59794533+AbdurRahmanMashoorAli@users.noreply.github.com>
2022-11-15 08:45:54 +0530

    Update WeatherForecastController.cs

--- Image_Manipulation_Api/Controllers/WeatherForecastController.cs
+++ Image_Manipulation_Api/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ namespace Image_Manipulation_Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : Controller
     {
         private static readonly string[] Summaries = new[]
         {
